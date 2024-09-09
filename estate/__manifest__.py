2b4515470b315fb2d88030d4096d9fbd4aefd4b8
Luis Jorge Prieto Favela <44274243+Luiskop@users.noreply.github.com>
2024-09-09 17:44:24 -0600

    Update __manifest__.py

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,7 +5,7 @@
     "depends": ["base"],  # dependencies
     "data": [
         "security/ir.model.access.csv",
-        #"views/estate_property_views.xml",
+        "views/estate_property_views.xml",
         "views/estate_menus.xml",
     ],
     "installable": True,
